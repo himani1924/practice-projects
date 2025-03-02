01e355128009469fadc4fd2396b6b9f2a7e46b6d
Himani <himani1@tothenew.com>
2025-03-02 16:21:13 +0530

    made some changes

--- promptopia/app/api/auth/[...nextauth]/route.js
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import { connectToDB } from "@/utils/database";
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
+import User from "@/models/user";
 
 const handler = NextAuth({
     providers:[
@@ -10,24 +11,41 @@ const handler = NextAuth({
             // scope: 'profile'
         })
     ],
-    async session({session}) {
-        
-    },
-    async signIn({profile}) {
-        // every next.js route is a serverless route
-        try {
-            await connectToDB()
-            // check if the user exists in db 
-
-            // if notFound, create new user 
+    callbacks:{
+        async session({session}) {
+            const sessionUser = await User.findOne({email: session.user.email})
+            session.user.id = sessionUser._id.toString()
+            return session
+        },
+        async signIn({profile}) {
+            // every next.js route is a serverless route
+            try {
+                await connectToDB()
+                // check if the user exists in db 
+                const userExists = await User.findOne({email: profile.email})
+                if(userExists){
+                    console.log('user exists');
+                }
+                // if notFound, create new user 
+                if(!userExists){
+                    console.log('user does not exist');
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(' ','').toLowerCase(),
+                        image:profile.picture
+                    },
+                )
+                    console.log('new user created');
+                }
+                return true
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
             
-            return true
-        } catch (error) {
-            console.log(error);
-            return false;
         }
-        
     }
+    
 })
 
 export {handler as GET, handler as POST}
